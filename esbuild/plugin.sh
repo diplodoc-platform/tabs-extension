0fab2f093003242cdaf2e6ac93be0906331d7b5b
vsesh <vsesh@yandex-team.ru>
2023-06-27 17:05:31 +0300

    a little fix

--- esbuild/plugin.sh
+++ esbuild/plugin.sh
@@ -1,5 +1,4 @@
 esbuild src/plugin/index.ts --outfile=plugin/index.js \
   --bundle --platform=node \
   --sourcemap \
-  --external:markdown-it --external:node:* \
   --define:PACKAGE="\"$(node -pe "require('./package.json').name")\""
