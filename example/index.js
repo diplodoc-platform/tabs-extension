8f0be45b762490cb6907fc5dc7c41e905eb87944
vsesh <vsesh@yandex-team.ru>
2023-06-27 14:31:16 +0300

    tmp

--- example/index.js
+++ example/index.js
@@ -18,9 +18,7 @@ import {readFile} from 'node:fs/promises';
 <html>
     <head>
         ${result.meta.script.map((scriptFile) => `<script src="${scriptFile}"></script>`)}
-        ${result.meta.style.map(
-            (styleFile) => `<link rel="stylesheet" href="${styleFile}"></script>`,
-        )}
+        ${result.meta.style.map((styleFile) => `<link rel="stylesheet" href="${styleFile}" />`)}
     </head>
     <body>
         ${result.html}    
