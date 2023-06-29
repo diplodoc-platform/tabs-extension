e5f3e33e0c769ae8f8dbc1d8a4d13add2abf5c4e
vsesh <vsesh@yandex-team.ru>
2023-06-29 13:02:30 +0300

    a little fix

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
-    moduleDirectories: ['node_modules', 'src'],
     transform: {
         '^.+\\.(j|t)s?$': ['ts-jest', {tsconfig: '<rootDir>/test/tsconfig.json'}],
     },
