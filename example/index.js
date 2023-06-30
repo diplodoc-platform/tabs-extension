44876f38636854a31aa13ae6b1785ad4da7ef7f5
vsesh <vsesh@yandex-team.ru>
2023-06-30 14:07:24 +0300

    Update example

--- example/index.js
+++ example/index.js
@@ -19,6 +19,12 @@ import {readFile} from 'node:fs/promises';
     <head>
         ${result.meta.script.map((scriptFile) => `<script src="${scriptFile}"></script>`)}
         ${result.meta.style.map((styleFile) => `<link rel="stylesheet" href="${styleFile}" />`)}
+        <script>
+            window.diplodocTabs.addEventListener('selecttab', (event) => {
+                const {group, key} = event.detail;
+                console.log(\`Tabs with key=\${key} in group=\${group} were selected!\`);
+            });
+        </script>
     </head>
     <body>
         ${result.html}    
