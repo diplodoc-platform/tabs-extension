9ecdcb6a89be3c5990fe89386bf4f498a17573c8
vsesh <vsesh@yandex-team.ru>
2023-07-25 15:16:12 +0300

    remove inline css

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,6 +1,5 @@
 import {GLOBAL_SYMBOL} from '../common';
 import {TabsController} from './TabsController';
-import './scss/tabs.scss';
 
 if (typeof window !== 'undefined' && typeof document !== 'undefined' && !window[GLOBAL_SYMBOL]) {
     window[GLOBAL_SYMBOL] = new TabsController(document);
